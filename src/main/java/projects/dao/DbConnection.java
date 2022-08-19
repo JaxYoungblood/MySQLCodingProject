ea6ac526ae623adf4cc23c11e9c5dd4916246f03
J Youngblood <jax3911@outlook.com>
2022-08-18 19:34:44 -0700

    first commit

---
+++ src/main/java/projects/dao/DbConnection.java
@@ -0,0 +1,33 @@
+package projects.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import projects.exception.DbException;
+
+public class DbConnection {
+	
+//Constants
+	private static final String SCHEMA = "projects";
+	private static final String USER = "projects";
+	private static final String PASSWORD = "projects";
+	private static final String HOST = "localhost";
+	private static final int PORT = 3306;
+
+		
+	public static Connection getConnection() {
+		String uri = String.format("jdbc:mysql://%s:%d/%s?user=%s&password=%s&useSSL=false", HOST, PORT, SCHEMA, USER, PASSWORD);
+			
+		System.out.println("Connecting with url: " + uri);
+			
+		try {
+			Connection conn = DriverManager.getConnection(uri);		//asked the driver to look up the connection for us
+			System.out.println("Successfully obtained connection!");//and asked driver to make the connection
+			return conn;
+		} catch (SQLException e) {
+			System.out.println("ERROR getting connection");
+			throw new DbException(e);
+		}
+	}
+}
